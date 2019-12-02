1e17b63f64fd28ec369660a6d9f3afbcecf44848
Dk3ys0p <roycamposvasquez@gmail.com>
2019-12-02 17:57:35 -0500

    first commit

---
+++ src/main/java/com/asdevp/galabooking/GalabookingApplication.java
@@ -0,0 +1,13 @@
+package com.asdevp.galabooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalabookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalabookingApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/asdevp/galabooking/GalabookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asdevp.galabooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalabookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
